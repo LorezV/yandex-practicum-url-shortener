54c0aa375b796cac37e90f474d071dbc20240f6d
Дмитрий Деркач <dima.derkach2004@gmail.com>
2023-01-27 15:12:56 +0500

    fix

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -8,7 +8,7 @@ var AppConfig Config
 
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
-	BaseUrl       string `env:"BASE_URL" envDefault:"http://127.0.0.1:8080"`
+	BaseURL       string `env:"BASE_URL" envDefault:"http://127.0.0.1:8080"`
 }
 
 func LoadAppConfig() {
